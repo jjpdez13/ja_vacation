a656f718ed2ba25b91cb328c23c521319174bd97
Johnny R Paredez III <jparedeziii@MacKenzie-2.local>
2024-10-14 11:21:48 -0500

    Server setup

---
+++ backend/routes/index.js
@@ -0,0 +1,25 @@
+// backend/routes/index.js
+const express = require("express");
+const router = express.Router();
+const apiRouter = require("./api");
+
+router.get("/hello/world", function (req, res) {
+  res.cookie("XSRF-TOKEN", req.csrfToken());
+  res.send("Hello World!");
+});
+
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    "XSRF-Token": csrfToken,
+  });
+});
+
+router.post("/api/test", function (req, res) {
+  res.json({ requestBody: req.body });
+});
+
+router.use("/api", apiRouter);
+
+module.exports = router;

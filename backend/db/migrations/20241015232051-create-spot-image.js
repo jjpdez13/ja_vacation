87dbf41ab442b9037e3f2919a94056ace16adb39
Johnny R Paredez III <jparedeziii@Mac.lan>
2024-10-16 16:41:06 -0500

    refactor migrations and seeders for table names to be in triple quotes for camelCase words

--- backend/db/migrations/20241015232051-create-spot-image.js
+++ backend/db/migrations/20241015232051-create-spot-image.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === "production") {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("SpotImages", {
+    await queryInterface.createTable('"SpotImages"', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -47,6 +47,6 @@ module.exports = {
     }, options, {});
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("SpotImages", options);
+    await queryInterface.dropTable('"SpotImages"', options);
   },
 };
